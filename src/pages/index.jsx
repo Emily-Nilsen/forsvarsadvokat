e1bd3270c6a9bb7f16f7b4c94a3701a3d369f3de
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-09-13 17:24:01 +0200

    rename hero component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { Hero } from '@/components/Hero'
-import { HeroMobile } from '@/components/HeroMobile'
 import { Fields } from '@/components/Fields'
 import { ReadMoreBox } from '@/components/ReadMoreBox'
 import { FeatureVideo } from '@/components/FeatureVideo'
@@ -27,12 +26,7 @@ export default function Home() {
       <Header />
 
       <main className="mx-auto max-w-screen-2xl sm:p-12 md:px-20 md:pb-20 md:pt-20 lg:pt-0">
-        <div className="">
-          <HeroMobile />
-        </div>
-        {/* <div className="hidden sm:block">
-          <Hero />
-        </div> */}
+        <Hero />
 
         <Fields />
 
