ab55ed506a18937a66136309cf00ff8f07ebe3cd
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-19 18:17:46 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,24 @@
-import Image from 'next/image'
-import Link from 'next/link'
-
-import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
-import { NavLinks } from '@/components/NavLinks'
 import { Contact } from '@/components/Contact'
-import useTranslation from 'next-translate/useTranslation'
 
 export function Footer() {
-  const { t, lang } = useTranslation('common')
-  const slogan = t('slogan')
-
   return (
     <footer className="bg-white">
       <Container>
-        <div className="flex flex-col items-start justify-between gap-y-12 pb-6 pt-16 lg:flex-row lg:items-center lg:py-16">
+        <div className="flex flex-col items-start justify-between pt-16 pb-6 gap-y-12 lg:flex-row lg:items-center lg:py-16">
           <Contact />
         </div>
-        <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
+        <div className="flex flex-col items-center pt-8 pb-12 border-t border-gray-200 md:flex-row-reverse md:justify-between md:pt-6">
           <div>
             <div className="flex items-center text-gray-900">
-              <Logo className="h-5 w-5 flex-none fill-gray-900" />
+              <Logo className="flex-none w-5 h-5 fill-gray-900" />
               <div className="ml-2">
                 <p className="font-serif text-lg font-bold uppercase">
                   Forsvarsadvokat
                 </p>
               </div>
             </div>
-            {/* <p className="mt-1 text-sm">{slogan}</p> */}
           </div>
           <p className="mt-6 text-sm text-gray-500 md:mt-0">
             &copy; {new Date().getFullYear()} Flemmen Johansen Media
