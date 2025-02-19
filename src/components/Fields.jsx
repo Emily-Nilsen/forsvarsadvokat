76bb7951bb213aaf57ac8085383a1a721a6a4ab4
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-19 18:33:05 +0300

    add oslo tinghus img

--- src/components/Fields.jsx
+++ src/components/Fields.jsx
@@ -1,48 +1,17 @@
 import Image from 'next/image'
-import {
-  CheckCircleIcon,
-  InformationCircleIcon,
-} from '@heroicons/react/20/solid'
 import { Expandable } from '@/components/Expandable'
 import { ReadMoreBox } from '@/components/ReadMoreBox'
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useSpring,
-  MotionValue,
-  useMotionValue,
-} from 'framer-motion'
+import { motion } from 'framer-motion'
 import useTranslation from 'next-translate/useTranslation'
 
 export function Fields() {
-  const { t, lang } = useTranslation('common')
-  const advokat = t('advokat')
+  const { t } = useTranslation('common')
   const about1 = t('about-1')
   const about1b = t('about-1b')
   const about2 = t('about-2')
   const about3 = t('about-3')
   const about4 = t('about-4')
   const lesMer = t('les-mer')
-
-  const fields = [
-    {
-      name: 1,
-      description: `${about1b}`,
-    },
-    {
-      name: 2,
-      description: `${about2}`,
-    },
-    {
-      name: 3,
-      description: `${about3}`,
-    },
-    {
-      name: 3,
-      description: `${about4}`,
-    },
-  ]
 
   return (
     <section>
@@ -51,10 +20,10 @@ export function Fields() {
           <>
             <div
               id="fagfelt"
-              className="overflow-hidden bg-white py-16 sm:pb-32 sm:pt-20"
+              className="py-16 overflow-hidden bg-white sm:pb-32 sm:pt-20"
             >
-              <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="px-6 mx-auto max-w-7xl lg:px-8">
+                <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                   <div className="lg:ml-auto lg:pl-4 lg:pt-4">
                     <div className="lg:max-w-lg">
                       <motion.p
@@ -69,7 +38,7 @@ export function Fields() {
                       >
                         {about1}
                       </motion.p>
-                      <dl className="mt-4 max-w-xl space-y-4 text-base leading-7 text-gray-600 lg:max-w-none">
+                      <dl className="max-w-xl mt-4 space-y-4 text-base leading-7 text-gray-600 lg:max-w-none">
                         <motion.div
                           whileInView={{ opacity: 1 }}
                           initial={{ opacity: 0 }}
