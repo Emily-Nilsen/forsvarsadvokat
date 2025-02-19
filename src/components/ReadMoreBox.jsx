708bbf8fd729f11db7d44121c98259d650ace7ac
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-19 18:37:42 +0300

    clean up read more box comp

--- src/components/ReadMoreBox.jsx
+++ src/components/ReadMoreBox.jsx
@@ -1,20 +1,17 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import { motion } from 'framer-motion'
 import useTranslation from 'next-translate/useTranslation'
 
 export function ReadMoreBox() {
-  const { t, lang } = useTranslation('common')
+  const { t } = useTranslation('common')
   const aboutTitle = t('about-title')
-  const about4 = t('about-4')
   const about5 = t('about-5')
   const about6 = t('about-6')
   const about7 = t('about-7')
 
   return (
-    <div className="bg-white pb-24 pt-6 sm:pb-32 sm:pt-16">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
+    <div className="pt-6 pb-24 bg-white sm:pb-32 sm:pt-16">
+      <div className="px-6 mx-auto max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto lg:mx-0 lg:max-w-none">
           <motion.h1
             whileInView={{ opacity: 1 }}
             initial={{ opacity: 0 }}
@@ -23,11 +20,11 @@ export function ReadMoreBox() {
               duration: 1,
               type: 'fade',
             }}
-            className="mt-2 font-display text-lg font-bold tracking-tight text-gray-900 sm:text-xl"
+            className="mt-2 text-lg font-bold tracking-tight text-gray-900 font-display sm:text-xl"
           >
             {aboutTitle}
           </motion.h1>
-          <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
+          <div className="grid max-w-xl grid-cols-1 gap-8 mt-10 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
             <div>
               <motion.p
                 whileInView={{ opacity: 1 }}
