3856a70df7021394515f0458f55d6cb691823c12
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2025-02-19 19:19:57 +0300

    video poster img

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,15 +2,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html className="h-full bg-white antialiased" lang="no">
+    <Html className="h-full antialiased bg-white" lang="no">
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/igq2srw.css" />
-        <link
-          rel="icon"
-          href="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Forsvarsadvokat/Forsvarsadvokat_asvr1q.svg"
-        />
+        <link rel="icon" href="/media/Forsvarsadvokat.svg" />
       </Head>
-      <body className="flex h-full flex-col">
+      <body className="flex flex-col h-full">
         <Main />
         <NextScript />
       </body>
